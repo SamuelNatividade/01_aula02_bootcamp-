64265f82e89e988720c960ad928b1e513f66e978
samuel_natividade <samuell.natividade@gmail.com>
2024-04-18 20:42:00 -0300

    exercicio 10

--- exercicios.py
+++ exercicios.py
@@ -34,9 +34,12 @@ print(resultado)
 # 8. Desenvolva um programa que calcule a potência de um número (base e expoente fornecidos pelo usuário).
 # 9. Faça um programa que converta a temperatura de Celsius para Fahrenheit.
 # 10. Escreva um programa que calcule a área de um círculo, recebendo o raio como entrada.
+import math 
+raio_do_circulo = float(input('Digite o raio: '))
+area_do_circulo = round(math.pi * raio_do_circulo ** 2,2)
+print(area_do_circulo)
 
-# #### Strings (`str`)
-
+##### Strings (`str`)
 # 11. Escreva um programa que receba uma string do usuário e a converta para maiúsculas.
 # 12. Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
 # 13. Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
