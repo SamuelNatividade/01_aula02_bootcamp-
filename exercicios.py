34defaa101b95edada63d19f91e1bcebc768554b
samuel_natividade <samuell.natividade@gmail.com>
2024-04-20 16:10:01 -0300

    adicionando exercicio2

--- exercicios.py
+++ exercicios.py
@@ -1,30 +1,34 @@
 ## string
-nome_aluno = 'samuel'
+# nome_aluno = 'samuel'
 
-print(type(nome_aluno))
-print(type('samuel'))
+# print(type(nome_aluno))
+# print(type('samuel'))
 
-print(nome_aluno.upper())
-print(nome_aluno.lower())
+# print(nome_aluno.upper())
+# print(nome_aluno.lower())
 
 
 ## booleans
-valor_1 = True 
-valor_2 = False
+# valor_1 = True 
+# valor_2 = False
 
-print(valor_1 and valor_2)
+# print(valor_1 and valor_2)
 
 
 # #### Inteiros (`int`)
 
 # 1. Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+numero_1 = int(input("Digite o primeiro numero: "))
+numero_2 = int(input("Digite o segundo numero: "))
+print(numero_1 + numero_2)
+
 # 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
 # 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
 # 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
-numero_01 = int(input('Inserir um numero inteiro:'))
-numero_02 = int(input('Inserir um numero inteiro:'))
-resultado = numero_01 // numero_02
-print(resultado)
+# numero_01 = int(input('Inserir um numero inteiro:'))
+# numero_02 = int(input('Inserir um numero inteiro:'))
+# resultado = numero_01 // numero_02
+# print(resultado)
 # 5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
 
 # #### Números de Ponto Flutuante (`float`)
@@ -34,16 +38,20 @@ print(resultado)
 # 8. Desenvolva um programa que calcule a potência de um número (base e expoente fornecidos pelo usuário).
 # 9. Faça um programa que converta a temperatura de Celsius para Fahrenheit.
 # 10. Escreva um programa que calcule a área de um círculo, recebendo o raio como entrada.
-import math 
-raio_do_circulo = float(input('Digite o raio: '))
-area_do_circulo = round(math.pi * raio_do_circulo ** 2,2)
-print(area_do_circulo)
+# import math 
+# raio_do_circulo = float(input('Digite o raio: '))
+# area_do_circulo = round(math.pi * raio_do_circulo ** 2,2)
+# print(area_do_circulo)
 
 ##### Strings (`str`)
 # 11. Escreva um programa que receba uma string do usuário e a converta para maiúsculas.
 # 12. Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
 # 13. Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
 # 14. Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
+# data = input('insira uma data no formato dd/mm/aaaa: ')
+# lista_data_separada = data.split('/')
+# print(f'Dia {lista_data_separada[0]} mes {lista_data_separada[1]} e ano {lista_data_separada[2]}')
+
 # 15. Escreva um programa que concatene duas strings fornecidas pelo usuário.
 
 # #### Booleanos (`bool`)
